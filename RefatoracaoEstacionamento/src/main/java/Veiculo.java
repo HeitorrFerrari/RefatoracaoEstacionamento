b76757ffa252bbfe3ff575b9779d19a6d6977bf0
Maria Eduarda Scomparin <108436308+duudafs@users.noreply.github.com>
2026-05-21 19:37:44 -0300

    exercicio 6

---
+++ RefatoracaoEstacionamento/src/main/java/Veiculo.java
@@ -0,0 +1,72 @@
+import java.time.LocalDateTime;
+
+public class Veiculo {
+
+    private String placa;
+    private String modelo;
+    private String cor;
+
+   private LocalDateTime dataHoraEntrada;
+   private LocalDateTime dataHoraSaidaPrevista;
+    private String seguradora;
+    private String numeroApolice;
+    private String telefoneAssistencia;
+
+    public Veiculo(String placa, String modelo, String cor,
+
+                   String seguradora, String numeroApolice, String telefoneAssistencia) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.seguradora = seguradora;
+        this.numeroApolice = numeroApolice;
+        this.telefoneAssistencia = telefoneAssistencia;
+        this.dataHoraEntrada = LocalDateTime.now();
+        this.dataHoraSaidaPrevista = LocalDateTime.now();
+    }
+
+    public void imprimirDados() {
+        System.out.println("Veículo: " + modelo);
+        System.out.println("Placa: " + placa);
+        System.out.println("Cor: " + cor);
+        System.out.println("Seguro: " + getDadosSeguroFormatados());
+    }
+
+    public String getDadosSeguroFormatados() {
+        return seguradora + " | Apólice " + numeroApolice + " | Assistência " + telefoneAssistencia;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+
+    public String getSeguradora() {
+        return seguradora;
+    }
+
+    public String getNumeroApolice() {
+        return numeroApolice;
+    }
+
+    public String getTelefoneAssistencia() {
+        return telefoneAssistencia;
+    }
+
+
+    public LocalDateTime getDataHoraEntrada() {
+        return dataHoraEntrada;
+    }
+
+    public LocalDateTime getDataHoraSaidaPrevista() {
+        return dataHoraSaidaPrevista;
+    }
+}
